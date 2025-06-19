90ba4b7fe4cb4f0341033818131989f8e4cf01d6
Rocio de Dios <rdiosalf@gmail.com>
2025-06-19 13:15:45 +0200

    cambiar sh por bat

--- vars/dockerLib.groovy
+++ vars/dockerLib.groovy
@@ -1,13 +1,13 @@
 def build(Map params){
-    sh "docker build -f ${params.DockerfilePath} -t ${params.DockerImage} ${params.DockerArgs} ${params.DockerContext}"
+    bat "docker build -f ${params.DockerfilePath} -t ${params.DockerImage} ${params.DockerArgs} ${params.DockerContext}"
 }
 
 def push(Map params){
-    sh "docker push ${params.DockerImage}"
+    bat "docker push ${params.DockerImage}"
 }
 
 def promoter(Map params){
-    sh "docker pull ${params.DockerImage}"
-    sh "docker tag ${params.DockerImage} ${params.DockerNewImage}"
-    sh "docker push ${params.DockerNewImage}"
+    bat "docker pull ${params.DockerImage}"
+    bat "docker tag ${params.DockerImage} ${params.DockerNewImage}"
+    bat "docker push ${params.DockerNewImage}"
 }
